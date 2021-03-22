9b0ff2bc7565a7a678683a4f7ff08d97c856e078
explore911 <darkshift911@gmail.com>
2021-03-22 18:21:52 +0000

    lab 2 done

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
+#include <string.h>
+#include <stdlib.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    int lenght = strlen(str);
+	char* temp;
+    temp = (char*) malloc(sizeof(char));
+    for(int i = 0; i<lenght/2; i++){
+        *temp = *(str+(lenght-i-1));
+        *(str+(lenght-i-1))=*(str+i);
+        *(str+i) = *temp;
+    }
 }
 
