646447271f36ed6584cf52fb161f0696d8b48a3f
explore911 <darkshift911@gmail.com>
2021-06-07 12:23:26 +0000

    Nu katay, sdavay

--- lab6/src/server.c
+++ lab6/src/server.c
@@ -20,18 +20,12 @@ struct FactorialArgs {
   uint64_t mod;
 };
 
-// uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
-//   uint64_t result = 0;
-//   a = a % mod;
-//   while (b > 0) {
-//     if (b % 2 == 1)
-//       result = (result + a) % mod;
-//     a = (a * 2) % mod;
-//     b /= 2;
-//   }
 
-//   return result % mod;
-// }
+
+
+
+
+
 
 uint64_t Factorial(const struct FactorialArgs *args) {
   uint64_t ans = 1;
@@ -86,7 +80,7 @@ int main(int argc, char **argv) {
       fprintf(stderr, "getopt returned character code 0%o?\n", c);
     }
   }
-
+   
   if (port == -1 || tnum == -1) {
     fprintf(stderr, "Using: %s --port 20001 --tnum 4\n", argv[0]);
     return 1;
